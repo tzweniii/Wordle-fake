df3d7c52fc3dfe829920047ca975e72a17734adc
tzweniii <tuananh09.dang@gmail.com>
2026-07-10 14:36:03 +0200

    loop in play_game for less code
    Refactored play_game function to use a loop for letter checking, reducing code duplication.

--- wordle.py
+++ wordle.py
@@ -50,83 +50,25 @@ def start_play():
 
  
 def play_game():
+
+    for letters in range(5):
 #first letter
-        if str(guess[0]) in todays_word:
-            print(f"the letter: {guess[0]} is in todays word")
-            if str(guess[0]) == str(todays_word[0]):
-                print(f'\033[32m{guess[0]} is in the right place\033[0m')
+        if str(guess[letters]) in todays_word:
+            print(f"the letter: {guess[letters]} is in todays word")
+            if str(guess[letters]) == str(todays_word[letters]):
+                print(f'\033[32m{guess[letters]} is in the right place\033[0m')
                 
                 #add the letter to guess word
-                guess_letters.append(guess[0])
+                guess_letters.append(guess[letters])
 
             else:
-                print(f'\033[33m{guess[0]} is in the wrong place\033[0m')
+                print(f'\033[33m{guess[letters]} is in the wrong place\033[0m')
         else:
-            print(f'\033[31mthere is no {guess[0]} in todays word\033[0m') 
-
-    #second letter
-        if str(guess[1]) in todays_word:
-            print("the letter:", guess[1], 'is in todays word')
-            if str(guess[1]) == str(todays_word[1]):
-                print(f'\033[32m{guess[1]} is in the right place\033[0m')
-
-                #add the letter to guess word
-                guess_letters.append(guess[1])
-
-            else:
-                print(f'\033[33m{guess[1]} is in the wrong place\033[0m')
-        else:
-            print(f'\033[31mthere is no {guess[1]} in todays word\033[0m') 
-
-    #third letter
-        if str(guess[2]) in todays_word:
-            print("the letter:", guess[2], 'is in todays word')
-            if str(guess[2]) == str(todays_word[2]):
-                print(f'\033[32m{guess[2]} is in the right place\033[0m')
-
-                #add the letter to guess word
-                guess_letters.append(guess[2])
-
-            else:
-                print(f'\033[33m{guess[2]} is in the wrong place\033[0m')
-        else:
-            print(f'\033[31mthere is no {guess[2]} in todays word\033[0m') 
-
-    #fourth letter
-        if str(guess[3]) in todays_word:
-            print("the letter:", guess[3], 'is in todays word')
-            if str(guess[3]) == str(todays_word[3]):
-                print(f'\033[32m{guess[3]} is in the right place\033[0m')
-
-                #add the letter to guess word
-                guess_letters.append(guess[3])
-
-            else:
-                print(f'\033[33m{guess[3]} is in the wrong place\033[0m')
-        else:
-            print(f'\033[31mthere is no {guess[3]} in todays word\033[0m') 
-
-    #fith letter
-        if str(guess[4]) in todays_word:
-            print("the letter:", guess[4], 'is in todays word')
-            if str(guess[4]) == str(todays_word[4]):
-                print(f'\033[32m{guess[4]} is in the right place\033[0m')
-
-                #add the letter to guess word
-                guess_letters.append(guess[4])
-
-            else:
-                print(f'\033[33m{guess[4]} is in the wrong place\033[0m')
-        else:
-            print(f'\033[31mthere is no {guess[4]} in todays word\033[0m')    
-
+            print(f'\033[31mthere is no {guess[letters]} in todays word\033[0m') 
                 
         if guess_letters == list(todays_word):
             print('WONNNNNNNNNNN')
             print('///////////////////////////')
-        
-
-            
 
 tries = 0
 while tries < 3:
